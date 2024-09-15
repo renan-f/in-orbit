d1c18462c3f290b54c5216e8d7f8719bfde223f3
Renan Andrade <filgueiras.rfa@gmail.com>
2024-09-14 23:13:11 -0300

    feat: criação de uma meta

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,7 +1,33 @@
 import fastify from "fastify";
+import {
+	serializerCompiler,
+	validatorCompiler,
+	type ZodTypeProvider,
+} from "fastify-type-provider-zod";
 
-const app = fastify();
+import { createGoal } from "../functions/create-goal";
+import z from "zod";
+
 const port = 3333;
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.post(
+	"/goals",
+	{
+		schema: {
+			body: z.object({
+				title: z.string(),
+				desiredWeeklyFrequency: z.number().int().min(1).max(7),
+			}),
+		},
+	},
+	async (request) => {
+		const { title, desiredWeeklyFrequency } = request.body;
+		await createGoal({ title, desiredWeeklyFrequency });
+	},
+);
 
 app
 	.listen({
