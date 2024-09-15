4dbfd8a647eeeb68fc5a6cebf74dbcdf50ef5ac9
Renan Andrade <filgueiras.rfa@gmail.com>
2024-09-15 19:02:29 -0300

    Cria método para buscar as metas da semana

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -5,13 +5,19 @@ import {
 	type ZodTypeProvider,
 } from "fastify-type-provider-zod";
 
-import { createGoal } from "../functions/create-goal";
 import z from "zod";
+import { createGoal } from "../functions/create-goal";
+import { getWeekPendingGoals } from "../functions/get-week-pending-goals";
 
 const port = 3333;
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
+
+app.get("/pending-goals", async () => {
+	const { pendingGoals } = await getWeekPendingGoals();
+	return { pendingGoals };
+});
 
 app.post(
 	"/goals",
