77c621c827b40ebf6761bde89f7c2b5b8d82a9f2
Renan Andrade <filgueiras.rfa@gmail.com>
2024-09-14 22:24:20 -0300

    ajuste nome coluna

--- server/src/db/seed.ts
+++ server/src/db/seed.ts
@@ -8,9 +8,9 @@ async function seed() {
 	const result = await db
 		.insert(goals)
 		.values([
-			{ title: "Acordar cedo", desiredWeedlyFrequency: 5 },
-			{ title: "Me exercitar", desiredWeedlyFrequency: 3 },
-			{ title: "Meditar", desiredWeedlyFrequency: 1 },
+			{ title: "Acordar cedo", desiredWeeklyFrequency: 5 },
+			{ title: "Me exercitar", desiredWeeklyFrequency: 3 },
+			{ title: "Meditar", desiredWeeklyFrequency: 1 },
 		])
 		.returning();
 
